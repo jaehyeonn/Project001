1eaa4cb51bf43a7eb6a07315b295407ccc8e9c78
ZEZE\a0104 <a01049469494@gmail.com>
2023-06-26 13:23:40 +0900

    feat: Add main screen v0.1.0

---
+++ Project/Start.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Project
+{
+    internal class Start
+    {
+        public void Scene()
+        {
+            WriteLine("press any key....");
+            ReadLine();
+            Clear();
+            WriteLine("겨울의 탑" +
+                "수십 년 동안 끝나지 않고 계속되고 있는 겨울," +
+                "인류는 마법과 증기 동력을 활용해 혹한에 맞서고" +
+                "있지만 상황은 날이 갈 수록 절망으로 쳐 닫고 있었다." +
+                "알려진 정보는 대륙 북쪽에 위치한 거대한 탑 위에" +
+                "그 원흉이 있다는 것뿐...." +
+                "" +
+                "그러나 원정을 떠난 이들은 대부분 돌아오지 못했고," +
+                "유일한 귀한자조차 탑에는 정체불명의 괴물과 악마들이" +
+                "득실대고 있다는 정보만을 전한 뒤 정신이 나가 버렸다." +
+                "인류는 거듭된 실패로 인해 크게 사기가 떨어졌지만," +
+                "다른 대안이 없기에 최후의 원정을 준비한다.");
+            
+        }
+    }
+}
